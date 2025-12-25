93f3add6574d02f8b75fcdae5276ecead9738e20
Zhanna <kezhanna.03@mail.ru>
2025-12-26 03:16:19 +0500

    task 5

--- firstElementFinder.js
+++ firstElementFinder.js
@@ -7,5 +7,7 @@ const findElement = (arr, func) => {
 }
 const test = findElement(["hello", "world", "javascript"], function (str) { return str.length > 5; })
 const test2 = findElement([1, 2, 3, 4], function(num) { return num > 2; })
+const test3 = findElement(["cat", "dog", "bird"], function(str) { return str.length > 10; })
 console.log(test)
 console.log(test2)
+console.log(test3)
