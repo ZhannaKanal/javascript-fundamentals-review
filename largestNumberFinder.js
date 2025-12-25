7f73c8d9ebd159e5dbaeacc32d4f031ea4726570
Zhanna <kezhanna.03@mail.ru>
2025-12-26 02:54:59 +0500

    task 5

--- largestNumberFinder.js
+++ largestNumberFinder.js
@@ -3,6 +3,8 @@ const largestOfAll = arr => {
   for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < arr[i].length; j++) {
 
+      const largeNumber = Math.max(...arr[i])
+   
     }
   }
 }
