45b5a83ee813d8ce10ca4ec4040106b245b0b497
Zhanna <kezhanna.03@mail.ru>
2025-12-26 02:55:39 +0500

    task 7

--- largestNumberFinder.js
+++ largestNumberFinder.js
@@ -1,12 +1,12 @@
-const largestOfAll = arr => {
-  let newArr = []
+const largestOfAll = (arr) => {
+  let newArr = [];
   for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < arr[i].length; j++) {
-
-      const largeNumber = Math.max(...arr[i])
+      const largeNumber = Math.max(...arr[i]);
       if (!newArr.includes(largeNumber)) {
-        newArr.push(largeNumber)
+        newArr.push(largeNumber);
       }
     }
   }
-}
+  return newArr;
+};
