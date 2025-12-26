7831800de64ed52ddf5accf25773ed22947b1bc8
Zhanna <kezhanna.03@mail.ru>
2025-12-26 22:44:45 +0500

    task 5

--- sliceAndSpliceAlgorithm.js
+++ sliceAndSpliceAlgorithm.js
@@ -1,7 +1,7 @@
 const frankenSplice = (arr1, arr2, index) => {
   const copied = [...arr2];
   for(let i=arr1.length-1; i>=0; i--){
-   
+    const spliced = copied.splice(index, 0, arr1[i]);
   }
   return copied
 }
