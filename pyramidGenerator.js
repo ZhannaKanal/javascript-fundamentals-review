389fe072549a82686caedfbffa132f1cf9e69ad8
Zhanna <kezhanna.03@mail.ru>
2025-12-27 23:50:34 +0500

    task 8

--- pyramidGenerator.js
+++ pyramidGenerator.js
@@ -1,12 +1,15 @@
-const pyramid = (strToRepeat, numOfRows, isFalse)=>{
-    let result = "\n";
-    if (!isFalse) {
-        for (let i = 0; i < numOfRows; i++){
-            const spaces = " ".repeat(numOfRows - i - 1);
-            const repeated = strToRepeat.repeat(2 * i + 1);
-            result += spaces + repeated + "\n";
-        }
-    } else {
-        for (let i = 0; i < numOfRows; i++){}
+const pyramid = (strToRepeat, numOfRows, isFalse) => {
+  let result = "\n";
+  if (!isFalse) {
+    for (let i = 0; i < numOfRows; i++) {
+      const spaces = " ".repeat(numOfRows - i - 1);
+      const repeated = strToRepeat.repeat(2 * i + 1);
+      result += spaces + repeated + "\n";
     }
-}
+  } else {
+    for (let i = 0; i < numOfRows; i++) {
+      
+    }
+  }
+  return result;
+};
