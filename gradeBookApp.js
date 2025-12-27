6b9d6f88d01f6a95581fd16f56a7fe0eb4b89a44
Zhanna <kezhanna.03@mail.ru>
2025-12-28 01:25:30 +0500

    task 7

--- gradeBookApp.js
+++ gradeBookApp.js
@@ -1,6 +1,7 @@
 const getAverage = (arr) => {
     let sum = 0;
     for (let i = 0; i < arr.length; i++) sum += arr[i];
+    return sum / arr.length;
 };
 const getGrade = (studentScore) => {};
 const hasPassingGrade = (score) => {};
