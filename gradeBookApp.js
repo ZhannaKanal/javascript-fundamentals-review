27a7a7a66c5cc83ca63f135008529e3035beccb0
Zhanna <kezhanna.03@mail.ru>
2025-12-28 01:26:18 +0500

    task 9

--- gradeBookApp.js
+++ gradeBookApp.js
@@ -5,6 +5,7 @@ const getAverage = (arr) => {
 };
 const getGrade = (score) => {
     if (score === 100) return "A+";
+    else if (score >= 90) return "A";
 };
 const hasPassingGrade = (score) => {};
 const studentMsg = (arrayOfScores, studentScore) => {};
