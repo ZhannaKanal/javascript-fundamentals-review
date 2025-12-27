0eae70fdb25ab58790b7879fe7edffe761ddc1b3
Zhanna <kezhanna.03@mail.ru>
2025-12-28 01:28:31 +0500

    task 13

--- gradeBookApp.js
+++ gradeBookApp.js
@@ -9,6 +9,7 @@ const getGrade = (score) => {
   else if (score >= 80) return "B";
   else if (score >= 70) return "C";
   else if (score >= 60) return "D";
+  else return "F";
 };
 const hasPassingGrade = (score) => {};
 const studentMsg = (arrayOfScores, studentScore) => {};
