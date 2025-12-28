0d37a5f893f64719f43e675516931970d4b2d258
Zhanna <kezhanna.03@mail.ru>
2025-12-29 00:18:24 +0500

    task 4

--- titleCaseConverter.js
+++ titleCaseConverter.js
@@ -1,4 +1,7 @@
-const titleCase = (str) => {
-  const newArr = [];
+const titleCase = str => {
+  const newArr = []
   const splitted = str.split(" ");
-};
+  for (let i = 0; i < splitted.length; i++) {
+    
+  }
+}
