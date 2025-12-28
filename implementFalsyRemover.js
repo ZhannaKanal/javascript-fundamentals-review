200eddf66e7bbd19d2be876fb0043e8ba2de3c85
Zhanna <kezhanna.03@mail.ru>
2025-12-29 02:20:32 +0500

    task 10

--- implementFalsyRemover.js
+++ implementFalsyRemover.js
@@ -10,3 +10,4 @@ const bouncer = (arr) => {
 };
 console.log(bouncer([7, "ate", "", false, 9]))
 console.log(bouncer([false, null, 0, NaN, undefined, ""]))
+console.log(bouncer([null, NaN, 1, 2, undefined]))
