65931755dc654bac46670c769e60b84ba5c8fb39
Zhanna <kezhanna.03@mail.ru>
2025-12-30 01:56:33 +0500

    task 7

--- inventoryManagementProgram.js
+++ inventoryManagementProgram.js
@@ -3,7 +3,9 @@ const inventory = [];
 const findProductIndex = (productName) => {
   const lowerCased = productName.toLowerCase();
   for (let i = 0; i < inventory.length; i++) {
-    
+    if (inventory[i].name.toLowerCase() === lowerCased) {
+      
+    }
   }
 };
 
