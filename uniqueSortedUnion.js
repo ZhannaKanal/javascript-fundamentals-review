1831c0ccc38d30d38b88eece204170ea7b801add
Zhanna <kezhanna.03@mail.ru>
2025-12-31 01:25:48 +0500

    task 4

--- uniqueSortedUnion.js
+++ uniqueSortedUnion.js
@@ -1,4 +1,8 @@
 const uniteUnique = (...args) => {
   const newArr = [];
-  for (let i = 0; i < args.length; i++) {}
+  for (let i = 0; i < args.length; i++) {
+    for (let j = 0; j < args[i].length; j++) {
+      
+    }
+  }
 };
