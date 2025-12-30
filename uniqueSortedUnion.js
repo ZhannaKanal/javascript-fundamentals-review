045997346f6622b5d5792e92cd93ee7a7a14dc23
Zhanna <kezhanna.03@mail.ru>
2025-12-31 01:27:17 +0500

    task 9

--- uniqueSortedUnion.js
+++ uniqueSortedUnion.js
@@ -9,4 +9,5 @@ const uniteUnique = (...args) => {
   }
   return newArr;
 };
-console.log(uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]))
+console.log(uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]));
+console.log(uniteUnique([1, 2, 3], [5, 2, 1, 4], [2, 1], [6, 7, 8]));
