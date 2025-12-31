c4f065fb00a041e7291be765da4b47586af74c9a
Zhanna <kezhanna.03@mail.ru>
2025-12-31 16:24:57 +0500

    task 11

--- implementElementSkipper.js
+++ implementElementSkipper.js
@@ -9,4 +9,5 @@ const dropElements = (arr, func)=> {
   return newArr
 }
 console.log(dropElements([1, 2, 3, 4], function (n) { return n >= 3; }))
+console.log(dropElements([1, 2, 3, 9, 2], function(n) {return n > 2;}))
 console.log(dropElements([0, 1, 0, 1], function(n) {return n === 1;}))
